4edde36301702c08ae767074d46ec287b56e641b
ambz <a.bandasith@gmail.com>
2022-07-25 22:00:08 -0700

    clean merge

---
+++ dataInterface/movies.js
@@ -0,0 +1,187 @@
+const { MongoClient } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId;
+
+const uri =
+"mongodb+srv://amber:Xuss6BVZiMdto0Aa@js330-test-cluster.l09khft.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+const databaseName = 'sample_mflix';
+const movieColl = 'movies'
+const commentColl = 'comments'
+
+module.exports = {}
+
+// ------------------------ //
+// --- movies interface --- //
+// ------------------------ //
+
+// https://www.mongodb.com/docs/drivers/node/current/usage-examples/find/
+module.exports.getAll = async () => {
+  const database = client.db(databaseName);
+  const movies = database.collection(movieColl);
+
+  const query = {};
+  let movieCursor = await movies.find(query).limit(10).project({title: 1}).sort({runtime: -1});
+
+  return movieCursor.toArray();
+}
+
+// https://www.mongodb.com/docs/drivers/node/current/usage-examples/findOne/
+module.exports.getById = async (movieId) => {
+  const database = client.db(databaseName);
+  const movies = database.collection(movieColl);
+  const query = {_id: ObjectId(movieId)};
+  let movie = await movies.findOne(query);
+
+  return movie;
+}
+
+module.exports.getByTitle = async (title) => {
+  const database = client.db(databaseName);
+  const movies = database.collection(movieColl);
+  const query = {title: title};
+  let movie = await movies.findOne(query);
+
+  return movie;
+}
+
+module.exports.getByIdOrTitle = async (identifier) => {
+  let movie;
+
+  if(ObjectId.isValid(identifier)){
+    movie = module.exports.getById(identifier);
+  } else {
+    movie = module.exports.getByTitle(identifier);
+  }
+
+  if(movie){
+    return movie;
+  } else {
+    return {error: `No item found with identifier ${identifier}.`}
+  }
+}
+
+// https://www.mongodb.com/docs/v4.4/tutorial/insert-documents/
+module.exports.create = async (newObj) => {
+  const database = client.db(databaseName);
+  const movies = database.collection(movieColl);
+
+  if(!newObj.title){
+    // Invalid movie object, shouldn't go in database.
+    return {error: "Movies must have a title."}
+  }
+  const result = await movies.insertOne(newObj);
+
+  if(result.acknowledged){
+    return { newObjectId: result.insertedId, message: `Item created! ID: ${result.insertedId}` }
+  } else {
+    return {error: "Something went wrong. Please try again."}
+  }
+}
+
+// https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/write-operations/change-a-document/
+module.exports.updateById = async (movieId, newObj) => {
+  const database = client.db(databaseName);
+  const movies = database.collection(movieColl);
+
+  // Product team says only these two fields can be updated.
+  const updateRules = {
+    $set: {"title" : newObj.title, "plot": newObj.plot}
+  };
+  const filter = { _id: ObjectId(movieId) };
+  const result = await movies.updateOne(filter, updateRules);
+
+  if(result.modifiedCount != 1){
+    return {error: `Something went wrong. ${result.modifiedCount} movies were updated. Please try again.`}
+  };
+
+  const updatedMovie = module.exports.getById(movieId);
+  return updatedMovie;
+}
+
+// https://www.mongodb.com/docs/drivers/node/current/fundamentals/crud/write-operations/delete/
+module.exports.deleteById = async (movieId) => {
+  const database = client.db(databaseName);
+  const movies = database.collection(movieColl);
+
+  const deletionRules = {_id:ObjectId(movieId)}
+  const result = await movies.deleteOne(deletionRules);
+
+  if(result.deletedCount != 1){
+    return {error: `Something went wrong. ${result.deletedCount} movies were deleted. Please try again.`}
+  };
+
+  return {message: `Deleted ${result.deletedCount} movie.`};
+}
+
+// -------------------------- //
+// --- comments interface --- //
+// -------------------------- //
+
+module.exports.getAllComments = async (movieId) => {
+  const database = client.db(databaseName);
+  const comments = database.collection(commentColl);
+
+  const query = {movie_id: ObjectId(movieId)}
+  let commentCursor = await comments.find(query);
+  return commentCursor.toArray();
+}
+
+module.exports.getOneComment = async (commentId) => {
+  const database = client.db(databaseName);
+  const comments = database.collection(commentColl);
+
+  const query = {_id: ObjectId(commentId)}
+  let commentCursor = await comments.find(query);
+  return commentCursor.toArray();
+}
+
+module.exports.createComment = async(movieId, newObj) =>{
+  // TODO: Validate that movieId is for an existing movie
+  const database = client.db(databaseName);
+  const comments = database.collection(commentColl);
+
+  const goodObj = {...newObj, movie_id: ObjectId(movieId), date: new Date()}
+
+  const result = await comments.insertOne(goodObj);
+
+  if(result.acknowledged){
+    return { newObjectId: result.insertedId, message: `Comment created! ID: ${result.insertedId}` }
+  } else {
+    return {error: "Something went wrong. Please try again."}
+  }
+}
+
+module.exports.updateCommentById = async (movieId, newObj) => {
+  const database = client.db(databaseName);
+  const comments = database.collection(commentColl);
+
+  // Product team says only these two fields can be updated.
+  const updateRules = {
+    $set: {"text" : newObj.text, "name": newObj.name}
+  };
+  const filter = { movie_id: ObjectId(movieId) };
+  const result = await comments.updateOne(filter, updateRules);
+
+  if(result.modifiedCount != 1){
+    return {error: `Something went wrong. Please try again.`}
+  };
+
+  const updatedComment = module.exports.getById(movieId);
+  return updatedComment;
+}
+
+module.exports.deleteCommentById = async (commentId) => {
+  const database = client.db(databaseName);
+  const movies = database.collection(commentColl);
+
+  const deletionRules = {_id:ObjectId(commentId)}
+  const result = await movies.deleteOne(deletionRules);
+
+  if(result.deletedCount != 1){
+    return {error: `Something went wrong. ${result.deletedCount} movies were deleted. Please try again.`}
+  };
+
+  return {message: `Deleted ${result.deletedCount} movie.`};
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,5 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/movies", require("./movies"));
-router.use("/comments", require("./comments"))
-router.use("/", (req, res) => res.status(404).send("Route not found. Available routes are /movies and /comments"))
+router.use("/", (req, res) => res.status(404).send("Route not found. Available routes are /movies"))
 module.exports = router;
