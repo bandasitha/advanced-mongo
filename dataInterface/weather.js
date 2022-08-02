5eec9b3f58b9636bf539a33c1394a14940ac26d5
ambz <a.bandasith@gmail.com>
2022-08-01 20:16:35 -0700

    more work on weather endpoints

--- dataInterface/weather.js
+++ dataInterface/weather.js
@@ -10,3 +10,39 @@ const databaseName = 'sample_weatherdata';
 const dataCollection = 'data'
 
 module.exports = {}
+
+module.exports.getByCallLetter = async (callLetter) => {
+    const database = client.db(databaseName);
+    const weatherData = database.collection(dataCollection);
+    const query = {callLetters: ObjectId(callLetter)};
+    let weatherCursor = await weatherData.findOne(query);
+  
+    return weatherCursor.toArray();
+  }
+
+  module.exports.getAll = async () => {
+    const database = client.db(databaseName);
+    const weatherData = database.collection(dataCollection);
+  
+    const query = {};
+    let weatherCursor = await weatherData.find(query).limit(10);
+  
+    return weatherCursor.toArray();
+  }
+
+  module.exports.create = async (newObj) => {
+    const database = client.db(databaseName);
+    const weatherData = database.collection(dataCollection);
+  
+    if(!newObj.elevation){
+      // Invalid movie object, shouldn't go in database.
+      return {error: "Movies must have a title."}
+    }
+    const result = await weatherData.insertOne(newObj);
+  
+    if(result.acknowledged){
+      return { newObjectId: result.insertedId, message: `Item created! ID: ${result.insertedId}` }
+    } else {
+      return {error: "Something went wrong. Please try again."}
+    }
+  }
--- routes/weather.js
+++ routes/weather.js
@@ -2,3 +2,40 @@ const { Router } = require("express");
 const router = Router();
 
 const weatherData = require('../dataInterface/weather');
+
+router.get("/:callLetters", async (req, res) => {
+    let weather = await weatherData.getByCallLetter(eq.params.id)
+  
+    if (weather) {
+      res.status(200).send(weather)
+    } else {
+      res.status(500).send({ error: "Something went wrong. Please try again." })
+    }
+  });
+  
+  
+  router.get("/", async (req, res, next) => {
+    let movieList = await weatherData.getAll()
+  
+    if (movieList) {
+      res.status(200).send(movieList)
+    } else {
+      // If movieList is empty/null, something serious is wrong with the MongoDB connection.
+      res.status(500).send({ error: "Something went wrong. Please try again." })
+    }
+  });
+  
+  router.post("/", async (req, res) => {
+    let resultStatus;
+    let result = await weatherData.create(req.body);
+  
+    if (result.error) {
+      resultStatus = 400;
+    } else {
+      resultStatus = 200;
+    }
+  
+    res.status(resultStatus).send(result);
+  });
+
+  module.exports = router;
